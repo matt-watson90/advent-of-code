76f3ab0ed1528efec1ef8c988e48de31c2c4f9e9
matthewwatson <matthew.watson@mottmac.com>
2017-12-04 18:16:47 +0000

    Another small refactor to reuse the listOfInt code

--- problem1.hs
+++ problem1.hs
@@ -2,14 +2,15 @@ import Data.Char
 
 
 solvePart1 :: Integer -> Int
-solvePart1 x = doTheCalculation listOfInt 1
-    where listOfInt = map digitToInt $ show x
+solvePart1 x = doTheCalculation (listOfInt x) 1
 
 solvePart2 :: Integer -> Int
-solvePart2 x = doTheCalculation  listOfInt gap
-    where listOfInt = map digitToInt $ show x
-          gap = (length listOfInt) `div` 2
+solvePart2 x = doTheCalculation list gap
+    where list = (listOfInt x)  
+          gap = (length list) `div` 2
 
+listOfInt :: Integer -> [Int]
+listOfInt x =  map digitToInt $ show x
 
 doTheCalculation :: [Int] -> Int-> Int
 doTheCalculation xs gap = totalForGapN circularList gap
