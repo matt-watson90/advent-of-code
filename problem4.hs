cf7d4456ce2ac701921e260e2571090fabc49062
matthewwatson <matthew.watson@mottmac.com>
2017-12-05 16:24:45 +0000

    Fix indentation

--- problem4.hs
+++ problem4.hs
@@ -11,12 +11,11 @@ main2 = do
   filecontent <- readFile "problem4-input.txt"
   putStrLn (show $ length $ filter containsNoDuplicates $ map sortEachString $ convertToIndividualWords $ lines filecontent)
 
-  convertToIndividualWords :: [String] -> [[String]]
-  convertToIndividualWords xs = map (splitOn " ") xs
-  
-  containsNoDuplicates :: [String] -> Bool
-  containsNoDuplicates xs = xs == nub xs
-  
-  sortEachString :: [String] -> [String]
-  sortEachString xs = map sort xs
-  
+convertToIndividualWords :: [String] -> [[String]]
+convertToIndividualWords xs = map (splitOn " ") xs
+
+containsNoDuplicates :: [String] -> Bool
+containsNoDuplicates xs = xs == nub xs
+
+sortEachString :: [String] -> [String]
+sortEachString xs = map sort xs
